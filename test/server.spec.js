01691319b03902dd9cce18bfb0827739164d3482
Jonathan Warner <jaxbot@gmail.com>
2016-03-17 18:50:40 -0400

    Switch to mocha

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,16 +1,16 @@
 var request = require('request');
 var config = require('./config');
-
+var assert = require('assert');
 describe("Backend Server", function() {
   it("responds to HTTP requests", function(done) {
     request(config.API_URL + '/api/ping', function(error, response, body) {
-      expect(body).toEqual("200");
+      assert.equal(body, "200");
       done();
     });
   });
   it("can talk to the database", function(done) {
     request(config.API_URL + '/api/health', function(error, response, body) {
-      expect(body).toEqual("200");
+      assert.equal(body, "200");
       done();
     });
   });
