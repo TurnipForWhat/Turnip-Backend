d505a424193139970441788ba42284906b280429
Jonathan Warner <jaxbot@gmail.com>
2016-04-03 16:35:14 -0400

    Fix user signup

--- api/user.js
+++ api/user.js
@@ -4,13 +4,13 @@ var bcrypt = require('bcrypt');
 module.exports = function(app) {
   app.post('/signup', function(req, httpRes) {
     var token = Math.random().toString(36);
-    hashPassword(req.body.password, function(err, res) {
-      db.query("INSERT INTO Users SET ?",
+    bcrypt.hash(req.body.password, 11, function(err, res) {
+      db.query("INSERT INTO User SET ?",
        { name: req.body.name,
          email: req.body.email,
          hashed_password: res,
          facebook_id: req.body.fbid,
-         login_token: loken,
+         login_token: token,
        }, function(err, res) {
          httpRes.send({ success: true, login_token: token });
        });
@@ -18,7 +18,7 @@ module.exports = function(app) {
   });
 
   app.post('/login', function(req, httpRes) {
-    db.query("SELECT * FROM Users WHERE email = ?", [req.body.email], function(err, res) {
+    db.query("SELECT * FROM User WHERE email = ?", [req.body.email], function(err, res) {
       bcrypt.compare(res[0].hashed_password, req.body.password, function(err, match) {
         if (err || !match) {
           httpRes.send({ success: false, reason: "invalid password" });
